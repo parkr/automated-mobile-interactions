9e00a1bb3b315ecbe1e7f2a65f1943e5da8241bb
Parker Moore <parkrmoore@gmail.com>
2013-12-18 23:14:38 -0500

    Don't minify ratchet js

--- js/ratchet.js
+++ js/ratchet.js
@@ -1,19 +1,4 @@
 ---
 ---
-
-{% capture le_js %}
-/*                  *\
- * $jquery-2.0.3.js *
-\*                  */
 {% include js/jquery-2.0.3.min.js %}
-/*                  *\
- * $ratchet.js *
-\*                  */
 {% include js/ratchet.js %}
-{% endcapture %}
-
-{% if site.GH_ENV %}
-  {{ le_js | strip_newlines | remove: '  ' }}
-{% else %}
-  {{ le_js }}
-{% endif %}
